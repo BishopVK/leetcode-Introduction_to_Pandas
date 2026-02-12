9d00db12293ad69b677669f5fc1c5373b8fec742
BishopVK <danielgraindorge@gmail.com>
2026-02-12 12:35:41 +0100

    Added ex02 finished

---
+++ ex02_display_the_first_three_rows/ex02.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+def selectFirstRows(employees: pd.DataFrame) -> pd.DataFrame:
+	# return employees.head(3)
+	
+	return employees[:3]
+
+if __name__ == "__main__":
+	data = [
+		[3, "Bob", "Operations", 48675],
+		[90, "Alice", "Sales", 11096],
+		[9, "Tatiana", "Engineering", 33805],
+		[60, "Annabelle", "InformationTechnology", 37678],
+		[49, "Jonathan", "HumanResources", 23793],
+		[43, "Khaled", "Administration", 40454],
+	]
+
+	df = pd.DataFrame(data, columns=["employee_id", "name", "department", "salary"])
+
+	result = selectFirstRows(df)
+	print(result)
