804e5844f3b578cc092f57d9e4c6b8bfa2b98cb6
BishopVK <danielgraindorge@gmail.com>
2026-02-13 02:24:37 +0100

    Added tests manipulating new column data

--- ex04_create_a_new_column/ex04.py
+++ ex04_create_a_new_column/ex04.py
@@ -1,7 +1,28 @@
 import pandas as pd
 
 def createBonusColumn(employees: pd.DataFrame) -> pd.DataFrame:
-	employees["bonus"] = employees["salary"] * 2
+	# employees["bonus"] = employees["salary"] * 2 # -> Salary * 2
+
+	# employees["bonus"] = employees["salary"] + 1000 # -> Salary + 1000
+
+	# employees["bonus"] = pow(employees["salary"], 2) # -> Square of Salary
+
+	""" employees["bonus"] = employees["salary"].where(  # -> Bono *2 if Salary > 5000, else 0
+		employees["salary"] > 5000,
+		0
+	) * 2 """
+
+	""" import numpy as np  # -> Bono *2 if Salary > 5000, else 0
+
+	employees["bonus"] = np.where(
+		employees["salary"] > 5000,
+		employees["salary"] * 2,
+		0
+	) """
+
+	employees["bonus"] = employees["salary"] * 2  # -> Bono *2 if Salary > 5000, else 0
+	employees.loc[employees["salary"] <= 5000, "bonus"] = 0
+
 	return employees
 
 if __name__ == "__main__":
