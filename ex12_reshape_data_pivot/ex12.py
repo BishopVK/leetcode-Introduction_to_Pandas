a233190d2c68f36c757b492b9f0739fd4b61058e
BishopVK <danielgraindorge@gmail.com>
2026-02-14 02:10:00 +0100

    Added ex12 finished

---
+++ ex12_reshape_data_pivot/ex12.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+def pivotTable(weather: pd.DataFrame) -> pd.DataFrame:
+	return weather.pivot(
+		index="month",
+		columns="city",
+		values="temperature"
+	)
+
+if __name__ == "__main__":
+	df = pd.DataFrame(
+		{
+			"city": ["Jacksonville", "Jacksonville", "Jacksonville", "Jacksonville", "Jacksonville", "ElPaso", "ElPaso", "ElPaso", "ElPaso", "ElPaso"],
+			"month": ["January", "February", "March", "April", "May", "January", "February", "March", "April", "May"],
+			"temperature": [13, 23, 38, 5, 34, 20, 6, 26, 2, 43],
+		}
+	)
+
+	result = pivotTable(df)
+	print(result)
